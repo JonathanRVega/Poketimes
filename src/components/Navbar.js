7de95a9bed0ca9164b20648ee73e22d0dac756af
JonathanRVega <Jonathaan.rv@gmail.com>
2020-11-17 14:09:03 -0700

    Fixed small bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,9 @@ const Navbar = (props) => {
     return (
         <nav className='nav-wrapper red darken-3'>
             <div className="container">
-            <a href="_" className="brand-logo">PokeTimes</a>
+            <a href="/Poketimes" className="brand-logo">PokeTimes</a>
                 <ul className="right">
-                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/Poketimes">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
